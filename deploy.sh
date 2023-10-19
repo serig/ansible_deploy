f4374ffbd54fba39c5f292b4268a11f5d9479c30
serig <serigne.ndiaye@outlook.com>
2023-10-19 18:27:48 +0000

    deploy.sh: ajout de l'entete options

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+###############################################################################
+#
+#   Description: déploiement à la voilée de conteneur docker
+#   
+#   Auteur: Serigne NDIAYE
+#
+#   Date: 19/10/2023
+#
+################################################################################
+
+echo "
+Options :
+    - --create : lancer des conteneurs
+    - --drop : supprimer les conteneurs par deploy.sh
+    - --infos : caractéristiques des conteneurs (ip, nom, user...)
+    - --start : redémarrage des conteneurs
+    - --ansible : déploiement arborescence ansible
+
+"
