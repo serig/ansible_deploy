ff2183e039c5be47c13a012826ba5f2c85749fc3
serig <serigne.ndiaye@outlook.com>
2023-10-25 13:05:20 +0000

    [DEV]: ajout des commentaires sur les conditions d'options

--- deploy.sh
+++ deploy.sh
@@ -10,31 +10,33 @@
 #
 ################################################################################
 
+# si option est create
 if [ "$1" == "--create" ];then
   echo ""
   echo " votre option est --create"
   echo ""
-
+# si option est drop
 elif [ "$1" == "--drop" ];then
   echo ""
   echo " votre option est --drop"
   echo ""
-
+#si option est infos
 elif [ "$1" == "--infos" ];then
 echo ""
 echo " votre option est --infos"
 echo ""
-
+# si option est start
 elif [ "$1" == "--start" ];then
 echo ""
 echo " votre option est --start"
 echo ""
-
+# si option est ansible
 elif [ "$1" == "--ansible" ];then
 echo ""
 echo " votre option est --ansible"
 echo ""
 
+#si aucune option
 else
 echo "
 Options :
